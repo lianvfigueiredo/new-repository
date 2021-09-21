498f7fe85ed25585c5d5cff42a8208d382b5543c
Lían V Figueiredo <lianvictor1@gmail.com>
2021-09-21 09:54:28 -0300

    Adicionando variável c

--- teste git/src/teste/Git.java
+++ teste git/src/teste/Git.java
@@ -4,8 +4,10 @@ public class Git {
 	public static void main(String[] args) {
 		int a = 0;
 		int b = 1;
+		int c = 2;
 		a += b;
 		System.out.println(a);
 		System.out.println(b);
+		System.out.println(c);
 	}
 }
